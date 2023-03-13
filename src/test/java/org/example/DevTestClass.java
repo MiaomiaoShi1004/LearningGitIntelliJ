7dd3d2f58f0d09120a847a85b9a374c4f7991bb8
MiaomiaoShi1004 <123462116+MiaomiaoShi1004@users.noreply.github.com>
2023-03-13 20:20:48 +0900

    Commit 2

--- src/test/java/org/example/DevTestClass.java
+++ src/test/java/org/example/DevTestClass.java
@@ -4,6 +4,5 @@ public class DevTestClass  {
     public static void main(String[] args) {
         System.out.println("DevClassTest");
 //        Miaomiao added this
-//        Miaomiao's commit 3
     }
 }
